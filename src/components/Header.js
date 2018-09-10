a908493240d61f16d50be2f584ca68d575c2a066
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-09-10 10:41:26 -0700

    navbar

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "./../images/logotest.png";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="header">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 col-md-6 offset-md-3 text-center">
+              <img src={logo} className="header__logo" alt="logo" />
+            </div>
+            <div className="col-md-3 d-none d-md-flex flex-column align-items-center justify-content-center">
+              <a href="tel:+1-541-318-0663" className="header__info pb-2">
+                Phone: 318-0663
+              </a>
+              <div className="header__info pb-2">Email: info@JPABS.net</div>
+              <div className="header__info">Hours: Mon-Fri 8am-5pm</div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-light bg-light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav d-flex justify-content-center">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Resources
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
