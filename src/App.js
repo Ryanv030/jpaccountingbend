e2508525c5fc611ec118c16f5f40d792aa0dcf9b
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-12-01 22:53:47 -0800

    code

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
         <Navbar />
+        <Header />
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,56 @@
 import React from "react";
-import logo from "./../images/logotest.png";
+import logo from "./../images/logo.png";
 
 const Header = () => {
   return (
     <div>
-      <header className="header">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-12 col-md-6 offset-md-3 text-center">
-              <img src={logo} className="header__logo" alt="logo" />
-            </div>
-            <div className="col-md-3 d-none d-md-flex flex-column align-items-center justify-content-center">
-              <a href="tel:+1-541-318-0663" className="header__info pb-2">
-                Phone: 541-318-0663
-              </a>
-              <div className="header__info pb-2">Email: info@JPABS.net</div>
-              <div className="header__info">Hours: Mon-Fri 8am-5pm</div>
-            </div>
+      <nav className="navbar navbar-expand-md navbar-light header">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src={logo} alt="logo" className="header__logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav mt-2 mt-lg-0">
+              <li className="nav-item active">
+                <a className="header__nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="header__nav-link" href="#">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="header__nav-link" href="#">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </header>
+      </nav>
     </div>
   );
 };
 
 export default Header;
+
+//  <div className="col-md-3 d-none d-md-flex flex-column align-items-center justify-content-center">
+//             <a href="tel:+1-541-318-0663" className="header__info pb-2">
+//               Phone: 541-318-0663
+//             </a>
+//             <div className="header__info pb-2">Email: info@JPABS.net</div>
+//             <div className="header__info">Hours: Mon-Fri 8am-5pm</div>
+//           </div>
